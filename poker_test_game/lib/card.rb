7f2a6190f69ec7b50831de1df6555d1388a2d480
David Gregorato <HydroSnake@gmail.com>
2021-09-15 13:31:12 +0900

    Put cards and the spec file in their respective folders.

---
+++ poker_test_game/lib/card.rb
@@ -0,0 +1,23 @@
+# Definition of a card
+
+class Card
+  attr_reader :suit, :rank
+
+  def initialize(suit:, rank:)
+    @suit = suit
+    @rank = case rank
+            when :jack then 11
+            when :queen then 12
+            when :king then 13
+            else rank
+            end
+  end
+
+  # def suit
+  #   @suit
+  # end
+
+  # def rank
+  #   @rank
+  # end
+end
---
+++ poker_test_game/spec/card_spec.rb
@@ -0,0 +1,39 @@
+# RSpec file for the playing cards.
+require 'card'
+
+RSpec.describe 'a playing card' do
+  it 'has a suit' do
+    raise unless Card.new(suit: :spades, rank: 4).suit == :spades
+  end
+
+  it 'has a rank' do
+    raise unless Card.new(suit: :spades, rank: 4).rank == 4
+  end
+
+  describe 'a jack' do
+    it 'ranks higher than 10' do
+      lower = Card.new(suit: :spades, rank: 10)
+      higher = Card.new(suit: :spades, rank: :jack)
+
+      raise unless higher.rank > lower.rank
+    end
+  end
+
+  describe 'a queen' do
+    it 'ranks higher than jack' do
+      lower = Card.new(suit: :spades, rank: :jack)
+      higher = Card.new(suit: :spades, rank: :queen)
+
+      raise unless higher.rank > lower.rank
+    end
+  end
+
+  describe 'a king' do
+    it 'ranks higher than queen' do
+      lower = Card.new(suit: :spades, rank: :queen)
+      higher = Card.new(suit: :spades, rank: :king)
+
+      raise unless higher.rank > lower.rank
+    end
+  end
+end
